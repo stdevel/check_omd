c33e4080c0cd3ebe47ec69519c3457cc92612eb6
Christian Stankowic <info@stankowic-development.net>
2016-06-27 08:12:23 +0200

    Release 0.1.1
    - added -w / --warning for excluding fragile services like npcd from
    throwing critical states
    - added hint when running plugin as root

--- check_omd.py
+++ check_omd.py
@@ -33,8 +33,14 @@ def getSiteStatus():
 			print "UNKNOWN: unable to check site: '{0}'".format(err.rstrip())
 		exit(3)
 	if res:
+		#try to find out whether omd was executed as root
+		if res.count("OVERALL") > 1:
+			print "UNKOWN: unable to check site, it seems this plugin is executed as root (use OMD site context!)"
+			exit(3)
+		
 		#check all services
 		fail_srvs=[]
+		warn_srvs=[]
 		if options.debug: print "DEBUG: Got result '{0}'".format(res)
 		for line in io.StringIO(res.decode('utf-8')):
 			service = line.rstrip().split(" ")[0]
@@ -42,16 +48,23 @@ def getSiteStatus():
 			if service not in options.exclude:
 				#check service
 				if status != "0":
-					fail_srvs.append(service)
-					if options.debug: print "{0} service has failed state ({1})".format(service, status)
+					if service in options.warning:
+						if options.debug: print "{0} service marked for warning has failed state ({1})".format(service, status)
+						warn_srvs.append(service)
+					else:
+						fail_srvs.append(service)
+						if options.debug: print "{0} service has failed state ({1})".format(service, status)
 			else:
 				if options.debug: print "Ignoring '{0}' as it's blacklisted...".format(service)
-		if len(fail_srvs) == 0:
+		if len(fail_srvs) == 0 and len(warn_srvs) == 0:
 			print "OK: OMD site '{0}' services are running.".format(site)
 			exit(0)
-		else:
+		elif len(fail_srvs) > 0:
 			print "CRITICAL: OMD site '{0}' has failed service(s): '{1}'".format(site, ' '.join(fail_srvs))
 			exit(2)
+		else:
+			print "WARNING: OMD site '{0}' has service(s) in warning state: '{1}'".format(site, ' '.join(warn_srvs))
+			exit(1)
 
 
 
@@ -60,13 +73,16 @@ if __name__ == "__main__":
 	desc='''%prog is used to check a particular OMD site status. By default, the script only checks a site's overall status. It is also possible to exclude particular services and only check the remaining services (e.g. rrdcached, npcd, icinga, apache, crontab).
 	
 	Checkout the GitHub page for updates: https://github.com/stdevel/check_omd'''
-	parser = OptionParser(description=desc,version="%prog version 1.0.0")
+	parser = OptionParser(description=desc,version="%prog version 1.1.0")
 	
 	#-d / --debug
 	parser.add_option("-d", "--debug", dest="debug", default=False, action="store_true", help="enable debugging outputs")
 	
 	#-e / --exclude
 	parser.add_option("-x", "--exclude", dest="exclude", default=["OVERALL"], action="append", metavar="SERVICE", help="defines one or more services that should be excluded")
+	
+	#-w / --warning
+	parser.add_option("-w", "--warning", dest="warning", default=[""], action="append", metavar="SERVICE", help="defines one or more services that only should throw a warning if not running (useful for fragile stuff like npcd)")
 	
 	#parse arguments
 	(options, args) = parser.parse_args()
